818ad9369cad7f5c1eab4ad736412719af754e2d
Alexander Magdibur <dezzzert.official@gmail.com>
2024-10-30 11:18:26 +0300

    Update LAB_9.cpp

--- LAB_9.cpp
+++ LAB_9.cpp
@@ -13,26 +13,30 @@ int main()
 	q = log(3 - pow(p, 2));
 	// Вычисление 1
 	y1 = (p * q - (2 * p - 2 * pow(p, 3)) / (3 - pow(p, 2))) / pow(q, 2);
+	cout << "Вычисление 1: " << y1 << endl;
 	// Вычисление 2
 	float up, down;
-	up = cos(x) * log(2 + pow(sin(x), 2)) - (2 * pow(sin(x), 2) * cos(x)) / (2 + pow(sin(x), 2));
+	up = cos(x) * log(2 + pow(sin(x), 2)) - ((2 * pow(sin(x), 2) * cos(x)) / (2 + pow(sin(x), 2)));
 	down = pow(log(2 + pow(sin(x), 2)), 2);
 	y2 = up / down;
+	cout << "Вычисление 2: " << y2 << endl;
+	cout << endl << endl;
 	// Вычисление в цикле
-	cout << "dX" << "        " << "dY/dX" << "        " << "|(dY/dX)-Y'|" << endl;
-	for (float dx = 0.0005; dx <= 0.016; dx = dx * 2)
+	cout << "Приращение dX" << "\t    " << "Производная" << "\t\t" << "Абс. ошибка" << "\t   " << "Относ. ошибка" << endl;
+	cout << "------------------------------------------------------------------------" << endl;
+	float dx = 0.0005;
+	float dx1, dx2, dy1, dy2, dy, derivative, abs_diff, relative;
+	while (dx <= 0.017)
 	{
-		float dx1 = x + dx / 2, dx2 = x - dx / 2;
-		float dy1_up = cos(dx1) * log(2 + pow(sin(dx1), 2)) - (2 * pow(sin(dx1), 2) * cos(dx1)) / (2 + pow(sin(dx1), 2));
-		float dy1_down = pow(log(2 + pow(sin(dx1), 2)), 2);
-		float dy1 = dy1_up / dy1_down;
-		float dy2_up = cos(dx2) * log(2 + pow(sin(dx2), 2)) - (2 * pow(sin(dx2), 2) * cos(dx2)) / (2 + pow(sin(dx2), 2));
-		float dy2_down = pow(log(2 + pow(sin(dx2), 2)), 2);
-		float dy2 = dy2_up / dy2_down;
-		float dy = dy1 - dy2;
-		float derivative = dy / dx;
-		float abs_diff = abs(derivative - y2);
-		cout << dx << "     " << derivative << "     " << abs_diff << endl;
+		dx1 = x + (dx / 2);
+		dx2 = x - (dx / 2);
+		dy1 = sin(dx1)/ log(2 + pow(sin(dx1), 2)); // Y(x + dx/2)
+		dy2 = sin(dx2) / log(2 + pow(sin(dx2), 2)); // Y(x - dx/2)
+		dy = dy1 - dy2;
+		derivative = dy / dx;
+		abs_diff = abs(derivative - y2);
+		relative = (abs_diff / y2) * 100;
+		cout << dx << "\t\t     " << derivative << "\t\t" << abs_diff << "\t   " << relative << " %" << endl;
+		dx = dx * 2;
 	}
-
 }
